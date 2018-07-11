bc389680e894c0736ebded18a88466dc9ab2bc84
Ewa Mikuła <ewamariamikula@gmail.com>
2018-07-11 18:53:34 +0200

    Add modal JS

--- js/index.js
+++ js/index.js
@@ -132,3 +132,36 @@ function gameFinished() { // funkcja kończąca grę po wskazanej przez gracza n
     }
     setGameElements();
 }
+(function(modal){ 	
+	var showModal = function(event){
+		event.preventDefault();
+		document.querySelector('#modal-overlay').classList.add('show');
+	};
+
+  var modalStart = document.querySelectorAll('.show-modal');
+	
+	for(var i = 0; i < modalStart.length; i++){
+		modalStart[i].addEventListener('click', showModal);
+	}
+	
+	var hideModal = function(event){
+		event.preventDefault();
+		document.querySelector('#modal-overlay').classList.remove('show');
+	};
+	
+	var closeButtons = document.querySelectorAll('.modal .close');
+	
+	for(var i = 0; i < closeButtons.length; i++){
+		closeButtons[i].addEventListener('click', hideModal);
+	} 
+	
+	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+	
+	var modals = document.querySelectorAll('.modal');
+	
+	for(var i = 0; i < modals.length; i++){
+		modals[i].addEventListener('click', function(event){
+			event.stopPropagation();
+		});
+		}
+})();
